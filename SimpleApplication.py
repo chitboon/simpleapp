96171a0f842914edfb17438c73690922fc2f6dc1
chitboon <chitboon@gmail.com>
2019-11-27 10:17:49 +0800

    Complete solution

--- SimpleApplication.py
+++ SimpleApplication.py
@@ -51,6 +51,54 @@ def retrieveUsers():
 
     return render_template('retrieveUsers.html', usersList=usersList, count=len(usersList))
 
+@app.route('/updateUser/<int:id>/', methods=['GET', 'POST'])
+def updateUser(id):
+    updateUserForm = CreateUserForm(request.form)
+    if request.method == 'POST' and updateUserForm.validate():
+        userDict = {}
+        db = shelve.open('storage.db', 'w')
+        userDict = db['Users']
+
+        user = userDict.get(id)
+        user.set_firstName(updateUserForm.firstName.data)
+        user.set_lastName(updateUserForm.lastName.data)
+        user.set_membership(updateUserForm.membership.data)
+        user.set_gender(updateUserForm.gender.data)
+        user.set_remarks(updateUserForm.remarks.data)
+
+        db['Users'] = userDict
+        db.close()
+
+        return redirect(url_for('retrieveUsers'))
+    else:
+        userDict = {}
+        db = shelve.open('storage.db', 'r')
+        userDict = db['Users']
+        db.close()
+
+        user = userDict.get(id)
+        updateUserForm.firstName.data = user.get_firstName()
+        updateUserForm.lastName.data = user.get_lastName()
+        updateUserForm.membership.data = user.get_membership()
+        updateUserForm.gender.data = user.get_gender()
+        updateUserForm.remarks.data = user.get_remarks()
+
+        return render_template('updateUser.html', form=updateUserForm)
+
+
+@app.route('/deleteUser/<int:id>', methods=['POST'])
+def deleteUser(id):
+    usersDict = {}
+    db = shelve.open('storage.db', 'w')
+    usersDict = db['Users']
+
+    usersDict.pop(id)
+
+    db['Users'] = usersDict
+    db.close()
+
+    return redirect(url_for('retrieveUsers'))
+
 
 if __name__ == '__main__':
     app.run()
